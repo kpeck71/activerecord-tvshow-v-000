5c4f4d00ca10342e91695a3b72d28ecd58f1a6fa
Kristen Peck <kpeck71@gmail.com>
2017-12-01 02:34:47 +0000

    automatically backed up by learn

--- db/migrate/001_create_shows.rb
+++ db/migrate/001_create_shows.rb
@@ -1,3 +1,7 @@
 class CreateShows < ActiveRecord::migration
-  create_table shows
+  create_table :shows do |t|
+    t.string :name
+    t.string :network
+    t.string :day
+    t.integer :rating
 end
